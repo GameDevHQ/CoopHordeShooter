394131fee60bbd30751fc5d53015d7437e4f7516
Valeryi Savich <relrin78@gmail.com>
2018-01-08 19:37:34 +0300

    Initial commit for UE4 project

---
+++ Source/CoopHordeShooter/CoopHordeShooterGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoopHordeShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPHORDESHOOTER_API ACoopHordeShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
