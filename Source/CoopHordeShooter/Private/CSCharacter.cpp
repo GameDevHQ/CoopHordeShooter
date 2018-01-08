6ec54e90b672b5d9d943112a0a1ab001e541dfa1
Valeryi Savich <relrin78@gmail.com>
2018-01-08 20:53:54 +0300

    Implemented third-person camera

--- Source/CoopHordeShooter/Private/CSCharacter.cpp
+++ Source/CoopHordeShooter/Private/CSCharacter.cpp
@@ -1,14 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "CSCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 
 // Sets default values
 ACSCharacter::ACSCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+    SpringArmComponent->bUsePawnControlRotation = true;
+    SpringArmComponent->SetupAttachment(RootComponent);
+
+    // Set the default camera for a character
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+    CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
 void ACSCharacter::MoveForward(float Value)
@@ -21,27 +30,22 @@ void ACSCharacter::MoveRight(float Value)
     AddMovementInput(GetActorRightVector() * Value);
 }
 
-void ACSCharacter::LookUp(float Value)
-{
-
-}
-
 // Called when the game starts or when spawned
 void ACSCharacter::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 }
 
 // Called every frame
 void ACSCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
 void ACSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
     // Bind WSAD keys 
     PlayerInputComponent->BindAxis("MoveForward", this, &ACSCharacter::MoveForward);
--- Source/CoopHordeShooter/Public/CSCharacter.h
+++ Source/CoopHordeShooter/Public/CSCharacter.h
@@ -6,26 +6,34 @@
 #include "GameFramework/Character.h"
 #include "CSCharacter.generated.h"
 
+
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class COOPHORDESHOOTER_API ACSCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
 public:
-	// Sets default values for this character's properties
-	ACSCharacter();
+    // Sets default values for this character's properties
+    ACSCharacter();
 
     // Keyboard moving WSAD
     void MoveForward(float Value);
     void MoveRight(float Value);
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UCameraComponent* CameraComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USpringArmComponent* SpringArmComponent;
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
