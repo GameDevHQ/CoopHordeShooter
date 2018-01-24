9d074187ee7d9719a3c021afdde4a2f7ff35242b
Valeryi Savich <relrin78@gmail.com>
2018-01-24 20:30:21 +0300

    Setup the Environment Query System (EQS)

---
+++ Source/CoopHordeShooter/Public/CSGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CSGameMode.generated.h"
+
+
+UCLASS()
+class COOPHORDESHOOTER_API ACSGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+};
