57a10357c6426f60c4077be2233842c3c06fcd20
Valeryi Savich <relrin78@gmail.com>
2018-01-25 21:08:35 +0300

    Added Wave-based spawning

--- Source/CoopHordeShooter/Private/CSGameMode.cpp
+++ Source/CoopHordeShooter/Private/CSGameMode.cpp
@@ -1,1 +1,43 @@
 #include "CSGameMode.h"
+#include "TimerManager.h"
+
+ACSGameMode::ACSGameMode():
+BotsToSpawn(0),
+WaveCount(0),
+TimeBetweenWaves(10.0f)
+{ 
+}
+
+void ACSGameMode::StartWave()
+{
+    WaveCount++;
+    BotsToSpawn = 2 * WaveCount;
+    GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ACSGameMode::SpawnBotTimerElapsed, 1.0f, true, 0.0f);
+}
+
+void ACSGameMode::EndWave()
+{
+    GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+}
+
+void ACSGameMode::PrepareForNextWave()
+{
+    GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ACSGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+void ACSGameMode::SpawnBotTimerElapsed()
+{
+    SpawnNewBot();
+
+    BotsToSpawn--;
+    if (BotsToSpawn <= 0)
+    {
+        EndWave();
+    }
+}
+
+void ACSGameMode::StartPlay()
+{
+    Super::StartPlay();
+    PrepareForNextWave();
+}
--- Source/CoopHordeShooter/Public/CSGameMode.h
+++ Source/CoopHordeShooter/Public/CSGameMode.h
@@ -9,4 +9,25 @@ UCLASS()
 class COOPHORDESHOOTER_API ACSGameMode : public AGameModeBase
 {
     GENERATED_BODY()
+public:
+    ACSGameMode();
+protected:
+    
+    UFUNCTION(BlueprintImplementableEvent, Category="GameMode")
+    void SpawnNewBot();
+
+    UPROPERTY(EditDefaultsOnly, Category="GameMode", meta=(ClampMin=0))
+    float TimeBetweenWaves;
+
+    void StartWave();
+    void EndWave();
+    void PrepareForNextWave();
+    void SpawnBotTimerElapsed();
+
+    int BotsToSpawn;
+    int WaveCount; 
+    FTimerHandle TimerHandle_BotSpawner;
+    FTimerHandle TimerHandle_NextWaveStart;
+public:
+    virtual void StartPlay() override;
 };
